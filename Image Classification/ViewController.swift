996a79d8ca3b32c155bfd5c86c04c9bd639aebb2
Besar <b3sar11@gmail.com>
2018-08-26 16:21:38 +0200

    Project create

---
+++ Image Classification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image Classification
+//
+//  Created by Test on 8/26/18.
+//  Copyright © 2018 B3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
